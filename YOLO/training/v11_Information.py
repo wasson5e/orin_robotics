70f3ec47bb42c3b8985f88f62de296be17cca106
Aaron Wasson <wasson.5e@gmail.com>
2025-10-16 15:34:36 -0500

    Missed Files

--- YOLO/training/v11_Information.py
+++
@@ -1,8 +0,0 @@
-#model information
-#Use on the conda environment yolo
-from ultralytics import YOLO
-
-model = YOLO("yolo11n.pt")
-
-for name, param in model.named_parameters():
-    print(name)
--- YOLO/training/v11_train.py
+++
@@ -1,6 +0,0 @@
-#Use on the conda environment yolo
-from ultralytics import YOLO
-
-model = YOLO("yolo11n.pt")
-
-results = model.train(data="/Users/awasson/Documents/GitLab/orin-robotics/YOLO/dataset/sophia_dataset.yaml", epochs=30, imgsz=640, device='mps', freeze=23, batch=16)
